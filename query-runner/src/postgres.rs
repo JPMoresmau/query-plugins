8668f106495bc0fbac99da456264c3107e577bbc
JP Moresmau <jp@moresmau.fr>
2023-04-25 16:54:35 +0200

    Try a stream based approach

--- query-runner/src/postgres.rs
+++ query-runner/src/postgres.rs
@@ -1,8 +1,12 @@
+use std::pin::Pin;
+use std::task::Poll;
+
 use anyhow::{anyhow, Result};
-use futures_util::{pin_mut, TryStreamExt};
+use futures_util::{Stream, StreamExt};
+use pin_project::pin_project;
 use serde_yaml::Value;
 use tokio_postgres::types::{ToSql, Type};
-use tokio_postgres::{Config, NoTls};
+use tokio_postgres::{Client, Config, NoTls, RowStream, Statement};
 
 use crate::VariableResult;
 use crate::{
@@ -25,6 +29,19 @@ pub(crate) async fn execute(
     config: &Config,
     state: &mut ExecutionState,
 ) -> Result<Option<QueryResult>> {
+    let mut result = Option::None;
+    let mut stream = execute_stream(config, state).await?;
+    while let Some(res) = stream.next().await {
+        result = add_result(result, Some(res?));
+    }
+    Ok(result)
+}
+
+/// Execute a query and return the result.
+pub(crate) async fn execute_stream<'a>(
+    config: &Config,
+    state: &'a mut ExecutionState,
+) -> Result<Pin<Box<impl Stream<Item = Result<QueryResult>> + 'a>>> {
     // Get the query SQL.
     let query = state
         .query
@@ -42,49 +59,113 @@ pub(crate) async fn execute(
             eprintln!("connection error: {}", e);
         }
     });
-
     let stmt = client.prepare(&query).await?;
-    let columns = stmt.columns();
 
-    let it = client.query_raw(&stmt, &params).await?;
-    pin_mut!(it);
-    // final result.
-    let mut result = Option::None;
-    while let Some(row) = it.try_next().await? {
-        // Build row.
-        let mut result_one = Vec::with_capacity(columns.len());
-        for (ix, col) in columns.iter().enumerate() {
-            match col.type_() {
-                &Type::INT2 | &Type::INT4 | &Type::INT8 => result_one.push(Variable {
-                    name: col.name(),
-                    value: ValueResult::DataInteger(row.get(ix)),
-                }),
-                &Type::TEXT => result_one.push(Variable {
-                    name: col.name(),
-                    value: ValueResult::DataString(row.get(ix)),
-                }),
-                &Type::BOOL => result_one.push(Variable {
-                    name: col.name(),
-                    value: ValueResult::DataBoolean(row.get(ix)),
-                }),
-                &Type::FLOAT4 | &Type::FLOAT8 => result_one.push(Variable {
-                    name: col.name(),
-                    value: ValueResult::DataDecimal(row.get(ix)),
-                }),
-                _ => return Err(anyhow!("unsupported type {}", col.type_())),
-            }
-        }
-        // Send row to plugin.
-        let res = state.row(result_one)?;
-        result = add_result(result, res);
+    let st = QueryResultStream::new(state, client, stmt, &params).await?;
+    Ok(Box::pin(st))
+}
+
+#[pin_project]
+struct QueryResultStream<'a> {
+    state: &'a mut ExecutionState,
+    client: Client,
+    stmt: Statement,
+    #[pin]
+    it: RowStream,
+    at_end: bool,
+}
+
+impl<'a> QueryResultStream<'a> {
+    async fn new(
+        state: &'a mut ExecutionState,
+        client: Client,
+        stmt: Statement,
+        params: &[VariableResult],
+    ) -> Result<QueryResultStream<'a>> {
+        let it = client.query_raw(&stmt, params).await?;
+        Ok(QueryResultStream {
+            state,
+            client,
+            stmt,
+            it,
+            at_end: false,
+        })
     }
+}
 
-    // End.
-    let names: Vec<&str> = columns.iter().map(|c| c.name()).collect();
-    let end = state
-        .query
-        .execution_end(&mut state.store, &state.execution, &names)?;
-    Ok(add_result(result, end))
+impl<'a> Stream for QueryResultStream<'a> {
+    type Item = Result<QueryResult>;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        let this = self.project();
+        if *this.at_end {
+            return Poll::Ready(None);
+        }
+        match this.it.poll_next(cx) {
+            Poll::Ready(orow) => {
+                match orow {
+                    Some(Ok(row)) => {
+                        // Build row.
+                        let columns = this.stmt.columns();
+                        let mut result_one = Vec::with_capacity(columns.len());
+                        for (ix, col) in columns.iter().enumerate() {
+                            match col.type_() {
+                                &Type::INT2 | &Type::INT4 | &Type::INT8 => {
+                                    result_one.push(Variable {
+                                        name: col.name(),
+                                        value: ValueResult::DataInteger(row.get(ix)),
+                                    })
+                                }
+                                &Type::TEXT => result_one.push(Variable {
+                                    name: col.name(),
+                                    value: ValueResult::DataString(row.get(ix)),
+                                }),
+                                &Type::BOOL => result_one.push(Variable {
+                                    name: col.name(),
+                                    value: ValueResult::DataBoolean(row.get(ix)),
+                                }),
+                                &Type::FLOAT4 | &Type::FLOAT8 => result_one.push(Variable {
+                                    name: col.name(),
+                                    value: ValueResult::DataDecimal(row.get(ix)),
+                                }),
+                                _ => {
+                                    return Poll::Ready(Some(Err(anyhow!(
+                                        "unsupported type {}",
+                                        col.type_()
+                                    ))))
+                                }
+                            }
+                        }
+                        // Send row to plugin.
+                        match this.state.row(result_one).transpose() {
+                            Some(res) => Poll::Ready(Some(res)),
+                            None => {
+                                cx.waker().wake_by_ref();
+                                Poll::Pending
+                            },
+                        }
+                    }
+                    Some(Err(err)) => Poll::Ready(Some(Err(anyhow!(err)))),
+                    None => {
+                        *this.at_end = true;
+                        let columns = this.stmt.columns();
+                        let names: Vec<&str> = columns.iter().map(|c| c.name()).collect();
+                        Poll::Ready(
+                            this.state
+                                .query
+                                .execution_end(&mut this.state.store, &this.state.execution, &names)
+                                .map_err(|err| anyhow!(err))
+                                .transpose(),
+                        )
+                    }
+                }
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
 }
 
 impl ToSql for VariableResult {
